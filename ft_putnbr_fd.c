6b8ef4021b9ce60d010f0c466e73581e1c1b88ad
Chams AIDEL <chaidel@e1r12p2.42.fr>
2021-12-10 17:37:28 +0100

    all clear

---
+++ ft_putnbr_fd.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   putnbr_fd.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chaidel <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/28 13:54:41 by chaidel           #+#    #+#             */
+/*   Updated: 2021/12/08 15:51:46 by chaidel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	long int	nbr;
+
+	if (fd < 0)
+		return ;
+	nbr = n;
+	if (nbr < 0)
+	{
+		ft_putchar_fd('-', fd);
+		nbr *= (-1);
+	}
+	if (nbr > 9)
+	{
+		ft_putnbr_fd(nbr / 10, fd);
+		ft_putnbr_fd(nbr % 10, fd);
+	}
+	else
+		ft_putchar_fd(nbr + '0', fd);
+}
---
+++ ft_strjoin.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   strjoin.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chaidel <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/23 18:17:34 by chaidel           #+#    #+#             */
+/*   Updated: 2021/12/02 09:42:10 by chaidel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static	char	*ft_strcat(char *dest, const char *src)
+{
+	int	i;
+	int	n;
+
+	i = 0;
+	n = 0;
+	while (dest[n])
+		n++;
+	while (src[i])
+	{
+		dest[n + i] = src[i];
+		i++;
+	}
+	dest[n + i] = '\0';
+	return (dest);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str;
+	size_t	len1;
+	size_t	len2;
+
+	if (!s1 || !s2)
+		return (NULL);
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	str = (char *)ft_calloc((len1 + len2 + 1), sizeof(char));
+	if (!str)
+		return (NULL);
+	str = ft_strcat(str, s1);
+	str = ft_strcat(str, s2);
+	return (str);
+}
